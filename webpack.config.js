a616b8d5dbb734eadb22af026459d300bb693969
Gabrielle Sin <sin.gabrielle@gmail.com>
2017-12-10 21:05:21 -0800

    setup code: package json, boilerplate webpack config, main html, etc

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname,
+    filename: './dist/index.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+}; 
